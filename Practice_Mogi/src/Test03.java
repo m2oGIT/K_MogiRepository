615af786b9366915af6f2fb01b5f35a3970f9f0b
kyosuke <kyosuke_m@nifty.com>
2015-11-22 14:39:39 +0900

    Java001終了

--- Practice_Mogi/src/Test03.java
+++ Practice_Mogi/src/Test03.java
@@ -2,8 +2,10 @@
 public class Test03 {
 
   public static void main( String[] args ) {
-    System.out.println(args[0]);
-
+    System.out.println("2015年11月01日(Sun)");
+    System.out.println("Javaの演習問題です。");
+    System.out.println("eclipseで色々と試行錯誤しながら進めてみましょう。");
+    System.out.println("よろしくお願いします。");
   }
 
 }
