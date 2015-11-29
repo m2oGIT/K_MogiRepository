26b7a12e7eae732e05c7e7fc5f29a8976df314f1
kyosuke <kyosuke_m@nifty.com>
2015-11-29 14:01:09 +0900

    Java002のTest07,Test08をコミット

--- Practice_Mogi/src/Java002/Test07.java
+++ Practice_Mogi/src/Java002/Test07.java
@@ -1,14 +1,37 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+
 package Java002;
 
+/**
+ * @author EIS茂木 <br />
+ *         Test07 <br />
+ *         1-100までの数字を加算（ループを使用） <br />
+ *         更新履歴 2015/11/23 EIS茂木：新規作成 <br />
+ */
 public class Test07 {
 
+  /**
+   * Test07 <br />
+   * （説明） <br />
+   * 
+   * @param args コマンドライン引数
+   */
+  /**
+   * Test07 <br />
+   * 1-100までの数字を加算（ループを使用） <br />
+   * ３つのループの方法をそれぞれ使用する。 <br />
+   * 
+   * @param args コマンドライン引数
+   */
   public static void main( String[] args ) {
     // forを使用したループ
-    int sum = 0;
+    int sumFor = 0;
     for ( int i = 0; i < 100; i++ ) {
-      sum = sum + 1;
+      sumFor = sumFor + 1;
     }
-    System.out.println( "加算結果(for  )  ：" + sum );
+    System.out.println( "加算結果(for  )  ：" + sumFor );
 
     // whileを使用したループ
     int j = 0, sumWhile = 0;
--- Practice_Mogi/src/Java002/Test08.java
+++ Practice_Mogi/src/Java002/Test08.java
@@ -1,11 +1,29 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+
+package Java002;
+
+/**
+ * @author EIS茂木 <br />
+ *         Test08 <br />
+ *         1-100の整数のうち、奇数の場合のみ加算し、コンソールに出力する。 <br />
+ *         更新履歴 2015/11/23 EIS茂木：新規作成 <br />
+ */
 public class Test08 {
 
+  /**
+   * Test08 <br />
+   * 1-100の整数のうち、奇数の場合のみ加算し、コンソールに出力する。 <br />
+   * 
+   * @param args コマンドライン引数
+   */
   public static void main( String[] args ) {
 
     // forを使用したループ
     int sum = 0;
     for ( int i = 0; i < 100; i++ ) {
-      //奇数の場合のみ加算する。
+      // 奇数の場合のみ加算する。
       if ( i % 2 != 0 ) {
         sum = sum + 1;
       }
